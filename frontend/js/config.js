ca83e9022a789e7671769a4c9234469917ac403a
leongabess <leonardo25gabriel@hotmail.com>
2025-08-04 19:27:19 -0300

    correção de url do render

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -3,7 +3,7 @@ let apiBaseUrl;
 if (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1") {
   apiBaseUrl = "http://localhost:5000";
 } else {
-  apiBaseUrl = "https://taskmanager-gb90.onrender.com/"; 
+  apiBaseUrl = "https://taskmanager-gb90.onrender.com"; 
 }
 
 window.apiBaseUrl = apiBaseUrl;
